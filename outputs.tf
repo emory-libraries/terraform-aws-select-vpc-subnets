783f79b05cdd4f54732e1b936ab1e2e1313798c5
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-12 14:26:27 -0500

    Add additional output for id

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "vpc_subnet_arns" {
    value =  [for s in data.aws_subnet.selected : s.arn ]
 }
 
+output "vpc_subnet_ids" {
+  value = [for s in data.aws_subnet.selected : s.id ]
+}
+
 output "vpc_random_subnet" {
   value = data.aws_subnet.random
 }
